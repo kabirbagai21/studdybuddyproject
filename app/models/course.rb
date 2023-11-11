1823443a16e499c97f3227762ad78366e40db2ef
Brian Luna <b.luna@columbia.edu>
2023-11-10 20:41:51 -0500

    Update course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
   has_many :enrollments
   has_many :students, through: :enrollments
-  has_many :groups
+  has_many :groups, dependent: :destroy #ensures that when a course is deleted, all associated groups are also automatically deleted
 end
