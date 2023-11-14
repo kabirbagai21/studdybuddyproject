a28a4314c1541019a3dffdd4e6613ee46c992823
Brian Luna <b.luna@columbia.edu>
2023-11-14 16:25:59 -0500

    Update enrollment.rb

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,5 @@
 class Enrollment < ApplicationRecord
   belongs_to :student
   belongs_to :course
+  validates :student_id, uniqueness: { scope: :course_id, message: "should happen once per course" }
 end
