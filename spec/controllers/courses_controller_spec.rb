23875b7105b445b63d934625c4b576cd92ffc9d6
Brian Luna <b.luna@columbia.edu>
2023-11-14 15:02:25 -0500

    Create courses_controller_spec.rb

---
+++ spec/controllers/courses_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe CoursesController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  let(:course) { FactoryBot.create(:course) }
+  let(:student) { FactoryBot.create(:student) }
+  let(:group) { FactoryBot.create(:group, course: course) }
+
+  before do
+    sign_in student
+    student.groups << group
+  end
+
+  describe 'GET #show' do
+    it 'assigns the requested course and its details' do
+      get :show, params: { id: course.id }
+
+      expect(assigns(:course)).to eq(course)
+      expect(assigns(:enrolled_students)).to match_array(course.students)
+      expect(assigns(:groups)).to match_array(course.groups)
+      expect(assigns(:user_groups)).to match_array(student.groups.where(course: course))
+    end
+  end
+end
