c4a0e91d7006f673448780a748aaa245ee9b6172
Brian Luna <b.luna@columbia.edu>
2023-12-12 16:05:54 -0500

    Update group_spec.rb

--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
-require_relative '../app/models/group.rb'
 
 describe 'Group' do
+  let(:course) { FactoryBot.create(:course) }
+  let(:student1) { FactoryBot.create(:student, email: Faker::Internet.unique.email(domain: 'columbia.edu')) }
+  let(:student2) { FactoryBot.create(:student, email: Faker::Internet.unique.email(domain: 'columbia.edu')) }
+
   it 'contains valid attributes' do
-    course = Course.create(name: 'History')
     group = Group.new(course: course)
     expect(group).to be_valid
   end
@@ -14,39 +16,32 @@ describe 'Group' do
   end
 
   it 'belongs to a course' do
-    course = Course.create(name: 'History')
     group = course.groups.create
     expect(group.course).to eq(course)
   end
 
   it 'can have and belong to many students' do
-    course = Course.create(name: 'Mathematics')
     group = Group.create(course: course)
-    student1 = Student.create(name: 'Alice', email: 'alice@example.com', password: 'password1')
-    student2 = Student.create(name: 'Bob', email: 'bob@example.com', password: 'password2')
     group.students << student1
     group.students << student2
     expect(group.students).to include(student1, student2)
   end
 
   describe 'Group creation' do
-    let(:course) { Course.create(name: 'Mathematics') }
-    let(:student) { Student.create(name: 'John Doe', email: 'john@example.com', password: 'password') }
-
     it 'allows a user to create a group' do
-      group = Group.create(course_id: course.id, group_owner_id: student.id)
+      group = Group.create(course: course, group_owner_id: student1.id)
       expect(group).to be_valid
     end
 
     it 'does not allow a user to create more than one group' do
-      Group.create(course_id: course.id, group_owner_id: student.id)
-      second_group = Group.new(course_id: course.id, group_owner_id: student.id)
-      expect(second_group.valid?).to be_falsey
+      Group.create(course: course, group_owner_id: student1.id)
+      second_group = Group.new(course: course, group_owner_id: student1.id)
+      expect(second_group).not_to be_valid
     end
 
     it 'sets the group owner correctly' do
-      group = Group.create(course_id: course.id, group_owner_id: student.id)
-      expect(group.group_owner_id).to eq(student.id)
+      group = Group.create(course: course, group_owner_id: student1.id)
+      expect(group.group_owner_id).to eq(student1.id)
     end
   end
 end
