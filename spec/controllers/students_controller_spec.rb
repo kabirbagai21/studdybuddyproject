406f1a73c22b6a8056dd97307d8d1138bfc877c2
Brian Luna <b.luna@columbia.edu>
2023-12-12 03:51:28 -0500

    Update students_controller_spec.rb

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe StudentsController, type: :controller do
   include Devise::Test::ControllerHelpers
 
-  let(:student) { FactoryBot.create(:student) }
+  let(:student) { FactoryBot.create(:student, email: Faker::Internet.unique.email(domain: 'columbia.edu')) }
   let(:course) { FactoryBot.create(:course) }
   let(:enrolled_course) { FactoryBot.create(:course) }
 
@@ -19,10 +19,23 @@ RSpec.describe StudentsController, type: :controller do
   end
 
   describe 'GET #show' do
-    it 'assigns the requested student to @student' do
-      get :show
-      expect(assigns(:student)).to eq(student)
-      expect(assigns(:enrolled_courses)).to match_array(student.courses)
+    context 'when the student is not an instructor' do
+      it 'assigns the requested student to @student and enrolled courses' do
+        get :show, params: { id: student.id }
+        expect(assigns(:student)).to eq(student)
+        expect(assigns(:enrolled_courses)).to match_array(student.courses)
+      end
+    end
+
+    context 'when the student is an instructor' do
+      let(:instructor) { FactoryBot.create(:student, instructor: true, email: Faker::Internet.unique.email(domain: 'columbia.edu')) }
+
+      it 'assigns courses taught by the instructor' do
+        sign_in instructor
+        course.update(instructor_id: instructor.id)
+        get :show, params: { id: instructor.id }
+        expect(assigns(:courses_taught)).to include(course)
+      end
     end
   end
 
@@ -51,26 +64,23 @@ RSpec.describe StudentsController, type: :controller do
     end
 
     context 'when the student is already enrolled in the course' do
-      it 'redirects with a notice indicating already enrolled' do
-        enrolled_course.update(course_id: 123) # Example course_id
+      before do
         student.courses << enrolled_course
         student.save
-
-        post :enroll, params: { id: student.id, course_id: 123 }
-
+      end
+  
+      it 'redirects with a notice indicating already enrolled' do
+        post :enroll, params: { id: student.id, course_id: enrolled_course.course_id }
         expect(response).to redirect_to(student_path(student))
         expect(flash.to_hash).to include('notice' => 'You are already enrolled in this course.')
       end
     end
-
+  
     context 'when the student is not already enrolled in the course' do
       it 'enrolls the student and redirects with a success notice' do
-        course.update(course_id: 456) # Example course_id
-        post :enroll, params: { id: student.id, course_id: 456 }
-
-        student.reload
+        post :enroll, params: { id: student.id, course_id: course.course_id }
         expect(response).to redirect_to(student_path(student))
-        expect(student.courses).to include(course)
+        expect(student.reload.courses).to include(course)
         expect(flash.to_hash).to include('notice' => 'Successfully enrolled in the course.')
       end
     end
