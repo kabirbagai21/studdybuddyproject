aa95725aa069b03ad808defb5bdf6558f661c972
Brian Luna <b.luna@columbia.edu>
2023-12-12 15:44:25 -0500

    Update public_profiles_controller.rb

--- app/controllers/public_profiles_controller.rb
+++ app/controllers/public_profiles_controller.rb
@@ -1,6 +1,10 @@
 class PublicProfilesController < ApplicationController
   def show
-    @student = Student.find(params[:id])
-    @group = Group.find(params[:group_id]) if params[:group_id].present?
+    begin
+      @student = Student.find(params[:id])
+      @group = Group.find(params[:group_id]) if params[:group_id].present?
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, alert: "Student not found"
+    end
   end
 end
