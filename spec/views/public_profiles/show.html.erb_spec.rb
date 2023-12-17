f52f71c97659ae040504a7e6f764c8891e9427b5
Brian Luna <b.luna@columbia.edu>
2023-12-17 15:52:35 -0500

    Update show.html.erb_spec.rb
    # Assign @ courses

--- spec/views/public_profiles/show.html.erb_spec.rb
+++ spec/views/public_profiles/show.html.erb_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe "public_profiles/show.html.erb", type: :view do
   context "with a valid student and group" do
     let(:student) { FactoryBot.create(:student, bio: 'Test Bio', email: Faker::Internet.unique.email(domain: 'columbia.edu')) }
     let(:group) { FactoryBot.create(:group) }
+    let(:courses) { [FactoryBot.create(:course, name: "Course 1"), FactoryBot.create(:course, name: "Course 2")] }
 
     before do
       assign(:student, student)
       assign(:group, group)
+      assign(:courses, courses)  
 
       render
     end
@@ -20,11 +22,6 @@ RSpec.describe "public_profiles/show.html.erb", type: :view do
       expect(rendered).to include(student.email)
     end
 
-    # Removed the test for student's bio as it's not displayed in the view
-    # it "displays the student's bio" do
-    #   expect(rendered).to include(student.bio)
-    # end
-
     it "displays a link to the group page" do
       expect(rendered).to have_link('Return to Group Page', href: group_path(group))
     end
@@ -32,10 +29,12 @@ RSpec.describe "public_profiles/show.html.erb", type: :view do
 
   context "with a valid student and no group" do
     let(:student) { FactoryBot.create(:student, bio: 'Test Bio', email: Faker::Internet.unique.email(domain: 'columbia.edu')) }
+    let(:courses) { [FactoryBot.create(:course, name: "Course 1"), FactoryBot.create(:course, name: "Course 2")] }
 
     before do
       assign(:student, student)
       assign(:group, nil)
+      assign(:courses, courses)  
 
       render
     end
