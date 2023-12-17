eb9410c6c9a8838e09cbbebc8f4b54ab29beb0ed
kabirbagai21 <kbagai@ucsd.edu>
2023-12-17 15:29:55 -0500

    shows courses on public profile

--- app/controllers/public_profiles_controller.rb
+++ app/controllers/public_profiles_controller.rb
@@ -2,5 +2,6 @@ class PublicProfilesController < ApplicationController
   def show
     @student = Student.find(params[:id])
     @group = Group.find(params[:group_id]) if params[:group_id].present?
+    @courses = @student.courses
   end
 end
